55919b7cc48c1e1d0ee3f5cc78d0995912732713
Tobias Simetsreiter <tobias.simetsreiter@meliot.de>
2023-03-26 13:57:05 +0200

    test lua

--- build.zig
+++ build.zig
@@ -31,6 +31,10 @@ pub fn build(b: *std.Build) !void {
         .source_file = .{ .path = "src/spin.zig" },
     });
 
+    const lua = build_static_lua(b, target, optimize);
+
+    // @import("tprint.zig").TPrint(std.Build.Module);
+
     inline for (examples) |ex| {
         var exe: *std.build.CompileStep = b.addExecutable(.{
             .name = ex.name,
@@ -38,6 +42,10 @@ pub fn build(b: *std.Build) !void {
             .target = target,
             .optimize = optimize,
         });
+        exe.addIncludePath("libs/lua");
+        exe.addObject(lua);
+        exe.step.dependOn(&lua.step);
+
         inline for (modules) |mod| {
             const b_module = b.dependency(mod, .{}).module(mod);
             exe.addModule(mod, b_module);
@@ -58,4 +66,60 @@ pub fn build(b: *std.Build) !void {
         const run_step = b.step("run-" ++ ex.name, "Run the app in wasmtime");
         run_step.dependOn(&run.step);
     }
+}
+
+fn build_static_lua(b: *std.Build, target: anytype, optimize: anytype) *std.Build.CompileStep {
+    const lua_dir = "libs/lua/";
+    var lib = b.addObject(.{
+        .name = "lua",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibC();
+    lib.addIncludePath(lua_dir);
+    // lib.linkSystemLibrary("wasi-emulated-signal");
+
+    inline for (.{
+        "lapi.c",
+        "lauxlib.c",
+        "lbaselib.c",
+        "lcode.c",
+        "lcorolib.c",
+        "lctype.c",
+        "ldblib.c",
+        "ldebug.c",
+        // "ldo.c",
+        "ldump.c",
+        "lfunc.c",
+        "lgc.c",
+        "linit.c",
+        "liolib.c",
+        "llex.c",
+        "lmathlib.c",
+        "lmem.c",
+        "loadlib.c",
+        "lobject.c",
+        "lopcodes.c",
+        // "loslib.c",
+        "lparser.c",
+        "lstate.c",
+        "lstring.c",
+        "lstrlib.c",
+        "ltable.c",
+        "ltablib.c",
+        // "ltests.c",
+        "ltm.c",
+        "lua.c",
+        "lundump.c",
+        "lutf8lib.c",
+        "lvm.c",
+        "lzio.c",
+        // "onelua.c",
+    }) |f| {
+        lib.addCSourceFile(lua_dir ++ f, &.{
+            "-std=gnu99",
+            "-D_WASI_EMULATED_SIGNAL",
+        });
+    }
+    return lib;
 }
--- examples/json.zig
+++ examples/json.zig
@@ -4,6 +4,14 @@ var stdout = std.io.getStdOut().writer();
 
 const spin = @import("spin");
 
+// const lua = @import("lua");
+
+const lua = @cImport({
+    @cInclude("lua.h");
+    @cInclude("lualib.h");
+    @cInclude("lauxlib.h");
+});
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -14,6 +22,20 @@ pub fn main() !void {
     try spin.response.send(stdout);
 
     var env = try std.process.getEnvMap(arena.allocator());
+
+    // const info = @typeInfo(lua);
+    // inline for (info.Struct.decls) |decl| {
+    //     std.log.debug("Decl: {s}", .{decl.name});
+    // }
+
+    var L: *lua.lua_State = undefined;
+    lua.luaL_openlibs(L, 1);
+    // _ = lua.luaopen_base(L);
+    // _ = lua.luaopen_table(L);
+    // _ = lua.luaopen_io(L);
+    // _ = lua.luaopen_string(L);
+    // _ = lua.luaopen_math(L);
+
     const obj = .{
         .env = env.hash_map,
         .request = spin.request,
